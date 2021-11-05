1ad58801ff47b7cf6d71f3dac5d0344145d79244
Anh Khoa <dh51800321@student.stu.edu.vn>
2021-11-05 09:21:09 +0700

    AA

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,5 @@
 <?php 
 define('HOST', 'localhost:3306');
-define('DB', 'bookstore');
-define('USER', 'root');
-define('PW', '');
+define('DB', 'id17723055_bookstore');
+define('USER', 'id17723055_aa');
+define('PW', '123456');
