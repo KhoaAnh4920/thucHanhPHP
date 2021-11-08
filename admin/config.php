d183dea78e3515f7e0fb8edbb2ecdcc777e5b05b
Anh Khoa <dh51800321@student.stu.edu.vn>
2021-11-08 14:17:10 +0700

    updateThemSach

--- admin/config.php
+++ admin/config.php
@@ -3,6 +3,8 @@ define('HOST', 'localhost:3306');
 define('DB', 'bookstore');
 define('USER', 'root');
 define('PW', '');
+
+
 //define('HOST', 'localhost');
 //define('DB', 'id17723055_bookstore');
 //define('USER', 'id17723055_aa');
--- admin/store.php
+++ admin/store.php
@@ -14,7 +14,7 @@ if (isset($_FILES['hinhSach']))
     if ($_FILES['hinhSach']['error']==0) //ok
     {
         $h = $_FILES['hinhSach']['name'];
-        move_uploaded_file($_FILES['hinhSach']['tmp_name'], "imgages/book/$h");
+        move_uploaded_file($_FILES['hinhSach']['tmp_name'], "images/book/$h");
     }
 }
 
@@ -24,16 +24,19 @@ $sql="select nhaxb.manxb, loai.maloai from nhaxb, loai WHERE nhaxb.tennxb='$t' A
 $objStatement= $objPDO->prepare($sql);//return B
 $objStatement->execute();//ket qua truy van
 $data = $objStatement->fetchAll(PDO::FETCH_OBJ);
-var_dump($data);
+//var_dump($data);
 
-
-//SELECT nhaxb.manxb, loai.maloai FROM nhaxb, loai WHERE nhaxb.tennxb='Tổng Hợp Hồ Chí Minh' AND loai.tenloai='Tin Học'
 
 foreach($data as $k=>$row)
 {
-    $maxb = $row->manxb;
-    $maloai = $row->maloai;
+    $maxb = $row->manxb; // lấy mã nhà xuất bản
+    $maloai = $row->maloai; // lấy mã loại sách
 }
 
 // Insert //
+$sql="insert into sach(masach, tensach, mota, gia, hinh, manxb, maloai ) values(?, ?, ?, ?, ?, ?, ?) ";
+$a =[$m, $ts, $mota, $gia, $h, $maxb, $maloai];
+$objStatement= $objPDO->prepare($sql);//return B
+$objStatement->execute($a);//ket qua truy van
+header('location:index.php');
 
